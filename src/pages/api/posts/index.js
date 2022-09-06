36ebd221d4a90d3df10b827cc325e43a5e4f6464
Naruepanart Siangsanan <naruepanart1201@gmail.com>
2022-09-07 02:19:19 +0700

    Update index.js

--- src/pages/api/posts/index.js
+++ src/pages/api/posts/index.js
@@ -10,3 +10,22 @@ export async function get() {
     status: 200,
   });
 }
+
+export async function post({ request }) {
+  const data = await request.json();
+
+  const saveToDB = {
+    name: data.name,
+  };
+
+  const response = await fetchWithTimeout(
+    "https://jsonplaceholder.typicode.com/posts",
+    {
+      method: "POST",
+      body: JSON.stringify(saveToDB),
+    }
+  );
+  const responseData = await response.json();
+
+  return new Response(JSON.stringify(responseData), { status: 201 });
+}
